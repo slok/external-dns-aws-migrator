26c7565876e82d930d0481d5c3bee5fd109fc4b7
Xabier Larrakoetxea <slok69@gmail.com>
2018-06-20 20:40:05 +0200

    Add travis release upload on tag
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- hack/scripts/build.sh
+++ hack/scripts/build.sh
@@ -3,12 +3,31 @@
 set -o errexit
 set -o nounset
 
+src=./cmd/external-dns-aws-migrator
+out=./bin/external-dns-aws-migrator
 
-DIR="$( cd "$( dirname "${0}" )" && pwd )"
-ROOT_DIR=${DIR}/../..
-SRC=github.com/slok/external-dns-aws-migrator/cmd/external-dns-aws-migrator
-OUT=${ROOT_DIR}/bin/external-dns-aws-migrator
-LDF_CMP="-w -extldflags '-static'"
+goarch=amd64
 
-echo "Building binary at ${OUT}"
-CGO_ENABLED=0 go build -o ${OUT} --ldflags "${LDF_CMP}"  ${SRC}
+if [ $ostype == 'Linux' ]; then 
+    echo "Building linux release..."
+    goos=linux
+    binary_ext=-linux-amd64
+elif [ $ostype == 'Darwin' ]; then 
+    echo "Building darwin release..."
+    goos=darwin
+    binary_ext=-darwin-amd64
+elif [ $ostype == 'Windows' ]; then 
+    echo "Building windows release..."
+    goos=windows
+    binary_ext=-windows-amd64.exe
+else
+    echo "ostype env var required"
+    exit 1
+fi
+
+final_out=${out}${binary_ext}
+ldf_cmp="-w -extldflags '-static'"
+f_ver="-X main.Version=${VERSION:-dev}"
+
+echo "Building binary at ${final_out}"
+GOOS=${goos} GOARCH=${goarch} CGO_ENABLED=0 go build -o ${final_out} --ldflags "${ldf_cmp} ${f_ver}"  ${src}
